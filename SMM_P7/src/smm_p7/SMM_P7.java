4631ad252f814638832223fa669ac13a1327f534
pcerezo <cerezotrabajo7@correo.ugr.es>
2020-04-01 21:08:59 +0200

    Comienzo el diseño de la práctica 7.

---
+++ SMM_P7/src/smm_p7/SMM_P7.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smm_p7;
+
+/**
+ *
+ * @author pabloc
+ */
+public class SMM_P7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    /*public static void main(String[] args) {
+        // TODO code application logic here
+    }*/
+    
+}
